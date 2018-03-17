97976179dce6fee2597ad3d4b65063ca1dee44a7
Carolynn Vansant <cvansant@MacBook-Pro-5.fios-router.home>
2018-03-16 22:08:08 -0400

    created methods to standardize capitalization and separation characters

--- src/main/java/io/zipcoder/ItemParser.java
+++ src/main/java/io/zipcoder/ItemParser.java
@@ -2,22 +2,33 @@ package io.zipcoder;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import static com.sun.org.apache.xalan.internal.xsltc.compiler.sym.PATTERN;
+import static java.util.regex.Pattern.CASE_INSENSITIVE;
+import static java.util.regex.Pattern.LITERAL;
 
 public class ItemParser {
 
+    public static int exceptionCounter;
+
 
     public ArrayList<String> parseRawDataIntoStringArray(String rawData){
+        String normalizedRawData = normalizeRawData(rawData);
         String stringPattern = "##";
-        ArrayList<String> response = splitStringWithRegexPattern(stringPattern , rawData);
+        ArrayList<String> response = splitStringWithRegexPattern(stringPattern , normalizedRawData);
         return response;
     }
 
     public Item parseStringIntoItem(String rawItem) throws ItemParseException{
+        ArrayList<String> itemPairs = new ArrayList<>();
+
         return null;
     }
 
     public ArrayList<String> findKeyValuePairsInRawItemData(String rawItem){
-        String stringPattern = "[;|^]";
+        String stringPattern = "[;]";
         ArrayList<String> response = splitStringWithRegexPattern(stringPattern , rawItem);
         return response;
     }
@@ -26,6 +37,49 @@ public class ItemParser {
         return new ArrayList<String>(Arrays.asList(inputString.split(stringPattern)));
     }
 
+
+    public String normalizeRawData(String rawData){
+        String normal = convertWeirdCharactersToSemiColon(rawData);
+        String normal2 = toLowerCaseString(normal);
+        return remove0FromCookie(normal2);
+
+    }
+
+    public String convertWeirdCharactersToSemiColon(String rawData) {
+        ArrayList<String> wierdCharacters = new ArrayList<>(Arrays.asList("@", "^", "!", "*", "%"));
+        Pattern pattern = Pattern.compile("@");
+        Matcher matcher = pattern.matcher(rawData);
+        String unWeird = matcher.replaceAll(";");
+        for (String character : wierdCharacters) {
+            matcher.reset(unWeird);
+            matcher.usePattern(Pattern.compile(character, Pattern.LITERAL));
+            unWeird = matcher.replaceAll(";");
+        }
+        return unWeird;
+    }
+
+    public String toLowerCaseString(String rawData){
+        ArrayList<String> upperCase = new ArrayList<>(Arrays.asList("A","B","C","D","E","F","G","H","I","J",
+                "K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"));
+        ArrayList<String> lowerCase = new ArrayList<>(Arrays.asList("a","b","c","d","e","f","g","h","i","j",
+                "k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"));
+        Pattern pattern = Pattern.compile("A");
+        Matcher matcher = pattern.matcher(rawData);
+        String lower = matcher.replaceAll("a");
+        for(int i = 0; i<upperCase.size(); i++){
+            matcher.reset(lower);
+            matcher.usePattern(Pattern.compile(upperCase.get(i)));
+            lower = matcher.replaceAll(lowerCase.get(i));
+        }
+        return lower;
+    }
+
+    public String remove0FromCookie(String rawData){
+        Pattern pattern = Pattern.compile("c..kie");
+        Matcher matcher = pattern.matcher(rawData);
+        String happySpelling = matcher.replaceAll("cookie");
+        return happySpelling;
+    }
 
 
 }
--- src/test/java/io/zipcoder/ItemParserTest.java
+++ src/test/java/io/zipcoder/ItemParserTest.java
@@ -14,7 +14,7 @@ public class ItemParserTest {
 
     private String rawSingleItemIrregularSeperatorSample = "naMe:MiLK;price:3.23;type:Food^expiration:1/11/2016##";
 
-    private String rawBrokenSingleItem =    "naMe:Milk;price:3.23;type:Food;expiration:1/25/2016##";
+    private String rawBrokenSingleItem =    "naMe:Milk;price:3.23;type:;expiration:1/25/2016##";
 
     private String rawMultipleItems = "naMe:Milk;price:3.23;type:Food;expiration:1/25/2016##"
                                       +"naME:BreaD;price:1.23;type:Food;expiration:1/02/2016##"
@@ -25,6 +25,7 @@ public class ItemParserTest {
     public void setUp(){
         itemParser = new ItemParser();
     }
+
 
     @Test
     public void parseRawDataIntoStringArrayTest(){
@@ -37,7 +38,8 @@ public class ItemParserTest {
     @Test
     public void parseStringIntoItemTest() throws ItemParseException{
         Item expected = new Item("milk", 3.23, "food","1/25/2016");
-        Item actual = itemParser.parseStringIntoItem(rawSingleItem);
+        String myNormalized = itemParser.normalizeRawData(rawSingleItem);
+        Item actual = itemParser.parseStringIntoItem(myNormalized);
         assertEquals(expected.toString(), actual.toString());
     }
 
@@ -56,7 +58,41 @@ public class ItemParserTest {
     @Test
     public void findKeyValuePairsInRawItemDataTestIrregular(){
         Integer expected = 4;
-        Integer actual = itemParser.findKeyValuePairsInRawItemData(rawSingleItemIrregularSeperatorSample).size();
+        String normalized = itemParser.normalizeRawData(rawSingleItemIrregularSeperatorSample);
+        Integer actual = itemParser.findKeyValuePairsInRawItemData(normalized).size();
         assertEquals(expected, actual);
     }
+
+    @Test
+    public void testNormalizeRawData(){
+        String initial = "naMe:Co0kie;price:3.23;type:Food^expiration:1/11/2016##";
+        String expected = "name:cookie;price:3.23;type:food;expiration:1/11/2016##";
+        String actual = itemParser.normalizeRawData(initial);
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testToLowerCaseItem(){
+        String expected = rawSingleItem.toLowerCase();
+        String actual = itemParser.toLowerCaseString(rawSingleItem);
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testConvertWeirdCharactersToSemiColon(){
+        String initial = "^!@*%";
+        String expected = ";;;;;";
+        String actual = itemParser.convertWeirdCharactersToSemiColon(initial);
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testRemove0FromCookie(){
+        String initial = "c00kie";
+        String expected = "cookie";
+        String actual = itemParser.remove0FromCookie(initial);
+        Assert.assertEquals(expected, actual);
+    }
+
+
 }
